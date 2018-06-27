f3ad45a5a1af0c0a8a588c79599f69ee70c0dc52
吉森瑠菜 <runa@yoshimori-no-MacBook-Pro.local>
2018-06-27 18:57:21 +0900

    create file

---
+++ iOS_b_2018/iOS_b_2018/RecommendViewController/RecommendViewController.swift
@@ -0,0 +1,35 @@
+//
+//  RecommendViewController.swift
+//  iOS_b_2018
+//
+//  Created by 吉森瑠菜 on 2018/06/27.
+//  Copyright © 2018年 KoYamaura. All rights reserved.
+//
+
+import UIKit
+
+class RecommendViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        // Do any additional setup after loading the view.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+    
+
+    /*
+    // MARK: - Navigation
+
+    // In a storyboard-based application, you will often want to do a little preparation before navigation
+    override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
+        // Get the new view controller using segue.destinationViewController.
+        // Pass the selected object to the new view controller.
+    }
+    */
+
+}
